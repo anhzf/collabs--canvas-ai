cd28ada1097d5d106bedf490f1ad209ad83147ca
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-10-03 22:58:57 +0700

    build: add Drawing canvas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <canvas
+      ref="cvs"
+      width="500"
+      height="500"
+      @mousemove="showAxis"
+    />
+    <div>X: {{ x }}</div>
+    <div>Y: {{ y }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld,
+  data: () => ({
+    ctx: CanvasRenderingContext2D,
+    x: 0,
+    y: 0,
+  }),
+
+  methods: {
+    drawLine(fromX: number, fromY: number, toX: number, toY: number) {
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = '#333';
+      this.ctx.lineWidth = 1;
+      this.ctx.moveTo(fromX, fromY);
+      this.ctx.lineTo(toX, toY);
+      this.ctx.stroke();
+      this.ctx.closePath();
+    },
+
+    showAxis(e: MouseEvent): void {
+      this.drawLine(this.x, this.y, e.offsetX, e.offsetY);
+      // Set the end path
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+  },
+
+  mounted() {
+    const el = this.$refs.cvs as HTMLCanvasElement;
+    const ctx = el.getContext('2d') as CanvasRenderingContext2D;
+
+    this.ctx = ctx;
   },
 })
 export default class Home extends Vue {}
